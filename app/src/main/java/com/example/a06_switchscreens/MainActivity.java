16a0cd3c1e4cb9d54d2e39e712677a5852bd7378
ChoeGyeongHu <khchoe248@gmail.com>
2019-12-15 20:47:46 +0900

    안드로이드 스튜디오에서  github 연습해보기

--- app/src/main/java/com/example/a06_switchscreens/MainActivity.java
+++ app/src/main/java/com/example/a06_switchscreens/MainActivity.java
@@ -9,6 +9,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    // test
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
